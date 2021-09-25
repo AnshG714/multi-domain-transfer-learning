b5673abc5b21f416dabedebefdb659f14ec96cde
Ansh Godha <ag759@cornell.edu>
2021-09-25 19:58:30 -0400

    Finish setup script

---
+++ setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+print_usage() {
+  echo "Use this script with either no flags or -s to skip downloads and unzipping."
+}
+
+s_flag=false
+
+while getopts 's' flag; do
+  case "${flag}" in
+    s) s_flag=true ;;
+    *) print_usage
+       exit 1;;
+  esac
+done
+
+# make the datasets directory if it doesn't exist.
+DATASET_DIR='./datasets'
+FLOWERS_DIR="$DATASET_DIR/flowers"
+CARS_DIR="$DATASET_DIR/cars"
+
+if [ ! -d "$DATASET_DIR" ]; then
+  mkdir $DATASET_DIR
+  mkdir $FLOWERS_DIR
+  mkdir $CARS_DIR
+fi
+
+# ------------- set up the flowers dataset --------------
+if [[ "$s_flag" = "false" ]]; then
+  wget https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/102flowers.tgz -P $FLOWERS_DIR
+  wget https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/imagelabels.mat -P $FLOWERS_DIR
+  wget https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/setid.mat -P $FLOWERS_DIR
+fi
+
+tar -xzf "$FLOWERS_DIR/102flowers.tgz" -C $FLOWERS_DIR
+mv "$FLOWERS_DIR/jpg" "$FLOWERS_DIR/images" 
+
+# -------------- set up the cars dataset ------------------
+if [[ "$s_flag" = "false" ]]; then
+  wget http://ai.stanford.edu/~jkrause/car196/cars_train.tgz -P $CARS_DIR
+  wget http://ai.stanford.edu/~jkrause/car196/cars_test.tgz -P $CARS_DIR
+  wget https://ai.stanford.edu/~jkrause/cars/car_devkit.tgz -P $CARS_DIR 
+fi
+
+tar -xzf "$CARS_DIR/car_devkit.tgz" -C $CARS_DIR
+tar -xzf "$CARS_DIR/cars_test.tgz" -C $CARS_DIR
+tar -xzf "$CARS_DIR/cars_train.tgz" -C $CARS_DIR
+
+wget http://ai.stanford.edu/~jkrause/car196/cars_test_annos_withlabels.mat -P "$CARS_DIR/devkit"
+rm "$CARS_DIR/devkit/cars_test_annos.mat"
+
+# ---------- Run utility script to set up annotations for pytorch ----------
+python ./utils/create-dataset.py
+
+
+
---
+++ utils/create-dataset.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import scipy.io
+import numpy as np
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+# np.set_printoptions(threshold=np.inf)
+
+DATA_PATH_PREFIX = "./datasets" #assuming we're running this from the project root.
+
+# load the flowers dataset -- [https://www.robots.ox.ac.uk/%7Evgg/data/flowers/102/]
+labels = scipy.io.loadmat(os.path.join(DATA_PATH_PREFIX, 'flowers/imagelabels.mat'))["labels"].flatten()
+split = scipy.io.loadmat(os.path.join(DATA_PATH_PREFIX, 'flowers/setid.mat'))
+
+# make the splits - each of these is an array (for some reason, the test and train labels have been split)
+train_split = split["tstid"].flatten()
+test_split = split["trnid"].flatten()
+val_split = split["valid"].flatten()
+
+def prepare_flowers_df(split):
+    """
+    split: A numpy array containing a list of image ids for this split
+    
+    Returns: A Pandas DataFrame containing 2 columns: img_name, which contains the image name in the form image_xxxxx.jpg, and a label signifying which flower it is.
+    """
+    
+    def image_name(n):
+        s = str(n)
+        return "0" * (5 - len(s)) + s + ".jpg"
+    
+    df = pd.DataFrame(columns=["img_name", "label"])
+    df["img_name"] = np.array([image_name(n) for n in split])
+    df["label"] = np.array([labels[n - 1] for n in split])
+    return df
+
+train_df = prepare_flowers_df(train_split)
+test_df = prepare_flowers_df(test_split)
+val_df = prepare_flowers_df(val_split)
+
+train_df.to_csv(os.path.join(DATA_PATH_PREFIX, 'flowers/train_csv.csv'), index = False, header=True)
+test_df.to_csv(os.path.join(DATA_PATH_PREFIX, 'flowers/test_csv.csv'), index = False, header=True)
+val_df.to_csv(os.path.join(DATA_PATH_PREFIX, 'flowers/val_csv.csv'), index = False, header=True)
+
+# print("Training set size for flowers", train_df.shape)
+# print("Testing set size for flowers", test_df.shape)
+# print("Validation set size for flowers", val_df.shape)
+
+# Now, we handle the cars dataset -- [https://ai.stanford.edu/~jkrause/cars/car_dataset.html]
+# NOTE: Both the test and validation set use the cars_test image directory
+train_split = scipy.io.loadmat(os.path.join(DATA_PATH_PREFIX, 'cars/devkit/cars_train_annos.mat'))["annotations"][0]
+res = []
+for el in list(train_split):
+    res.append(np.array(list(el)))
+train_split = np.array(res).reshape((-1, 6))
+
+test_and_val_split = scipy.io.loadmat(os.path.join(DATA_PATH_PREFIX, 'cars/devkit/cars_test_annos_withlabels.mat'))["annotations"][0]
+res = []
+for el in list(test_and_val_split):
+    res.append(np.array(list(el)))
+test_and_val_split = np.array(res).reshape((-1, 6))
+test_split, val_split = train_test_split(test_and_val_split, test_size = 0.5, random_state = 42)
+
+def prepare_cars_df(split):
+    """
+    split: In this case, it's a 2d numpy array who's second to last column is the label and the last column is the image name
+    """
+    df = pd.DataFrame(columns=["img_name", "label"])
+    df["img_name"] = split[:, -1]
+    df["label"] = split[:, -2]
+    return df
+
+train_df = prepare_cars_df(train_split)
+test_df = prepare_cars_df(test_split)
+val_df = prepare_cars_df(val_split)
+
+train_df.to_csv(os.path.join(DATA_PATH_PREFIX, 'cars/train_csv.csv'), index = False, header=True)
+test_df.to_csv(os.path.join(DATA_PATH_PREFIX, 'cars/test_csv.csv'), index = False, header=True)
+val_df.to_csv(os.path.join(DATA_PATH_PREFIX, 'cars/val_csv.csv'), index = False, header=True)
+
+# print("Training set size for cars", train_df.shape)
+# print("Testing set size for cars", test_df.shape)
+# print("Validation set size for cars", val_df.shape)
+
+
+
+
+
