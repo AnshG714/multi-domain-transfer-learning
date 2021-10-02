1ad815176660cc8340da61078e532e89487df57e
Ansh Godha <ag759@cornell.edu>
2021-10-02 00:24:42 -0400

    Basic NN ready; need to fix accuracy measure

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 from datasets import DomainDataset
 from utils.checkpoints import load_checkpoint, save_checkpoint
+from utils.evaluation_helpers import check_accuracy
 import transforms
 from model import Net
 
@@ -33,26 +34,32 @@ cars_val_ds = DomainDataset(
 cars_val = DataLoader(cars_val_ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
 
 
-def train_fn(loader, model, optimizer, loss_fn, scaler, device):
+def train_fn(loader, model, optimizer, loss_fn, device):
   for batch_idx, (data, targets) in enumerate(loader):
     # Get data to cuda if its available
     data = data.to(device=device)
     targets = targets.to(device=device)
 
-    with torch.cuda.amp.autocast():
-      scores = model(data)
-      loss = loss_fn(scores, targets.float())
+    scores = model(data)
+    print(targets)
+    print(targets.shape)
+    print(scores.shape)
+    loss = loss_fn(scores, targets.long())
 
-  optimizer.zero_grad()
-  scaler.scale(loss).backward()
-  scaler.step(optimizer)
-  scaler.update()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
 loss_fn = nn.CrossEntropyLoss()
-model = Net(net_version="b0", num_classes=102).to("cpu")
+model = Net(net_version="b0", num_classes=196).to("cpu")
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[20, 40], gamma=0.1)
 n_epochs = 100
+
+for _ in range(n_epochs):
+  train_fn(cars_train, model, optimizer, loss_fn, "cpu")
+  check_accuracy(cars_train, model, "cpu")
+
 
 
 
